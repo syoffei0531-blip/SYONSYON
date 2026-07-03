5480c3765c4ea0f65ec158d1a5f8c63b95f7c606
syoffei0531-blip <syoffei0531@gmail.com>
2026-07-03 11:08:38 +0900

    Update app.py

--- python/app.py
+++ python/app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return "Python API OK!"
 
+
+@app.route("/create-video", methods=["POST"])
+def create_video():
+    return {
+        "status": "OK",
+        "message": "create-video API is working!"
+    }
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
